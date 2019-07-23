ab8a1ec87112253a907aa217b2c69de9bfb5dc0c
open_mailbox <dev@open-mailbox.com>
2019-07-22 21:24:30 -0400

    Initial setup of static web assets.

--- __resource.lua
+++ __resource.lua
@@ -1,8 +1,18 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
+ui_page "web/index.html"
+
 client_scripts {
   "@NativeUI/NativeUI.lua",
   "client/client.lua"
+}
+
+files {
+  "web/index.html",
+  "web/img/radio.png",
+  "web/scripts/listener.js",
+  "web/styles/index.css",
+  "web/styles/reset.css"
 }
 
 dependencies {
